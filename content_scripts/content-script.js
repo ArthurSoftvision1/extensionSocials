d852487a38485f4135144375b687c6f0cfbd4520
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-09-26 09:36:10 +0300

    send message to the backgronud-script

--- content_scripts/content-script.js
+++ content_scripts/content-script.js
@@ -19,45 +19,70 @@ function removeEverything() {
 
 function addInput() {
   // adds input text to the page
-
-  var list = document.querySelector('#input-section');
+  
+  var list = document.querySelector('body');
   var input = document.createElement('input'); // creates input
+  input.style.margin = "20px";
+  
   input.setAttribute("id", "search-input");
   list.appendChild(input);
-  
   var value = document.getElementById('search-input').value;
+  var button = document.createElement('button');
+  button.setAttribute("id", "button");
+  list.appendChild(button);
+
+  var p = document.createElement('p');
+  p.textContent = 'Ajax';
+  p.style.margin = "0";
+  var buttonText = document.querySelector('#button');
+  buttonText.appendChild(p);
+
+  var resultsContainer = document.createElement('div');
+  resultsContainer.setAttribute("id", "results")
+  list.appendChild(resultsContainer);
+
+
 }
 
 function onChange() {
    // event handler for change input text
     var search = document.getElementById('search').value; // get the input value
+
+    browser.runtime.sendMessage({'input' : inputData}).then(sendMessage);
 }
 
-var button = document.getElementById('button');
-button.addEventListener('click', onChange, false);
-
-function addResponseToPage() {
-  // adds response ajax call to the page using jQuery
+// function addResponseToPage() {
+//   // adds response ajax call to the page using jQuery
   
-  browser.runtime.onMessage.addListener(request => {
-    browser.runtime.onMessage.addListener(social);
-    console.log(request.greeting);
-    return Promise.resolve({response: "Hi from content script"});
-  });
+//   browser.runtime.onMessage.addListener(request => { // receive message
+//     browser.runtime.onMessage.addListener(social);
+//     console.log(request.greeting);
+//     return Promise.resolve({response: "Hi from content script"});
+//   });
+// }
+////
 
+// function handleResponse(message) {
+//   console.log('background script sent a response: ${message.response}');
+//   browser.runtime.onMessage(social); // background script sent a response
+
+// }
+
+// function handleError() {
+//   console.log('Error: ${error}');
+// }
+
+function sendMessage(e) {
+  var data = document.getElementById('search-input').value;
+
+  var sending = browser.runtime.sendMessage({'social': data});
+  sending.then(handleResponse, handleError);
 }
 
-function insertSocial(socialUrl) { // here we insert the social image
-  var socialImage = document.createElement("img"); // create img
-  socialImage.setAttribute("src", socialUrl);
-  socialImage.setAttribute("style", "width: 100px"); // set the width
-  socialImage.setAttribute("style", "height: 100px"); // set the height
-  document.body.appendChild(socialImage); // append the social image
-}
+window.addEventListener("click", sendMessage);
 
-
-browser.runtime.sendMessage.addListener(social); // add social listener function
+browser.runtime.onMessage.addListener(social);
 
 addInput();
 onChange();
-addResponseToPage();
+// addResponseToPage();
